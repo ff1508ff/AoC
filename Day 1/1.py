bc4f0bdbd5464b5ffdb91c27cbbb2f2ca4267cab
F.Fuchs <111969821+ff1508ff@users.noreply.github.com>
2023-12-06 11:09:52 +0100

    Solution Day1

--- Day 1/1.py
+++ Day 1/1.py
@@ -1,6 +1,6 @@
 total = 0
 
-def find_and_add_numbers(line):
+def FindNumber(line):
     first_number = None
     last_number = None
 
@@ -9,16 +9,13 @@ def find_and_add_numbers(line):
             if first_number is None:
                 first_number = int(char)
             last_number = int(char)
-
-    return first_number, last_number
+    number = first_number*10 + last_number
+    return number
 
 with open('C:\dev\Project\AoC\Day 1\input.txt', 'r') as file:
     for line in file:
         line = line.strip()
-        first_number, last_number = find_and_add_numbers(line)
-        if first_number is not None:
-            total += first_number
-        if last_number is not None:
-            total += last_number
+        number = FindNumber(line)
+        total += number
 
 print(total)
