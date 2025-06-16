4a8ec67c8040d1335f159bf2f032d68ccb545e80
ff1508ff <111969821+ff1508ff@users.noreply.github.com>
2025-06-16 23:06:08 +0200

    Solution 2020 Day 2 Part 1

--- 2020/rust/src/bin/2-1.rs
+++ 2020/rust/src/bin/2-1.rs
@@ -4,37 +4,24 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("../inputs/2.txt").expect("Couldn't read the file");
 
-    let re_letter = Regex::new(r" (?<letter>\w+):").unwrap();
-    let re_min = Regex::new(r"(?<number>\w+)-").unwrap();
-    let re_max = Regex::new(r"-(?<number>\w+)").unwrap();
+    let re = Regex::new(r"(?<min>\d+)-(?<max>\d+)\s+(?<letter>\w):\s+(?<password>\w+)").unwrap();
+
     println!(
-        "{:?}",
+        "{}",
         input
             .lines()
-            .filter(|i| {
-                i.chars()
-                    .filter(|c| {
-                        *c == re_letter.captures(i).unwrap()["letter"]
-                            .chars()
-                            .next()
-                            .unwrap()
-                    })
-                    .count()
-                    >= re_min.captures(i).unwrap()["number"]
-                        .parse::<usize>()
-                        .unwrap()
-                    && i.chars()
-                        .filter(|c| {
-                            *c == re_letter.captures(i).unwrap()["letter"]
-                                .chars()
-                                .next()
-                                .unwrap()
-                        })
-                        .count()
-                        <= re_max.captures(i).unwrap()["number"]
-                            .parse::<usize>()
-                            .unwrap()
+            .filter(|line| {
+                if let Some(caps) = re.captures(line) {
+                    let min = caps["min"].parse::<usize>().unwrap();
+                    let max = caps["max"].parse::<usize>().unwrap();
+                    let letter = caps["letter"].chars().next().unwrap();
+                    let password = &caps["password"];
+                    let count = password.matches(letter).count();
+                    count >= min && count <= max
+                } else {
+                    false
+                }
             })
             .count()
-    );
+    )
 }
