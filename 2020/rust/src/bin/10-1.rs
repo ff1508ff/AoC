8d14e5642cb9843cd836372b1404d36750e73f20
ff1508ff <111969821+ff1508ff@users.noreply.github.com>
2025-06-24 15:50:21 +0200

    Rework 2020 Day 10 Part 1

--- 2020/rust/src/bin/10-1.rs
+++ 2020/rust/src/bin/10-1.rs
@@ -3,18 +3,21 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("../inputs/10.txt").expect("Couldn't read the file");
     let mut input: Vec<usize> = input.lines().map(|l| l.parse::<usize>().unwrap()).collect();
+
+    // I like this
+    input.push(0); // outlet
     input.sort_unstable();
+    input.push(input.last().unwrap() + 3); // laptop
+
     let mut jolt1 = 0;
     let mut jolt3 = 0;
-    let mut old = 0;
-    for number in input {
-        if 1 == number - old {
-            jolt1 += 1;
-        } else if 3 == number - old {
-            jolt3 += 1;
+
+    for pair in input.windows(2) {
+        match pair[1] - pair[0] {
+            1 => jolt1 += 1,
+            3 => jolt3 += 1,
+            _ => {}
         }
-        old = number;
     }
-    jolt3 += 1;
     println!("{}", jolt1 * jolt3);
 }
