c77a2b7c6b708591398fa4d15f95f5a0835348c0
ff1508ff <111969821+ff1508ff@users.noreply.github.com>
2025-07-02 22:22:32 +0200

    Tryed it... failed 2020 Day 18 Part 1

---
+++ 2020/rust/src/bin/18-1.rs
@@ -0,0 +1,9 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("../inputs/18.txt").expect("Couldn't read the file");
+    println!(
+        "I'm to stupid for this so I will come back wenn I know how to work with binary trees (or atleast I think that is what I'm missing)"
+    )
+}
+
